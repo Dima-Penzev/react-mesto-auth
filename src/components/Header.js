7062344bc48ba335e0a6cb5725b8e6f39c2e7264
Dima <D130188@yandex.ru>
2023-05-18 23:03:43 +0400

    feat: added useLocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import headerLogo from "../images/header-logo.svg";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 function Header({
   btnState,
@@ -8,7 +9,9 @@ function Header({
   resetStates,
   toggleBtnState,
 }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
   function signOut() {
     localStorage.removeItem("token");
@@ -16,38 +19,69 @@ function Header({
     resetStates();
   }
 
-  function renderBtn() {
-    if (btnState) {
-      return (
-        <Link className="header__entry" to="/signup" onClick={toggleBtnState}>
-          Регистрация
-        </Link>
-      );
-    } else if (loggedIn) {
-      return (
-        <Link className="header__entry" onClick={signOut}>
-          Выйти
-        </Link>
-      );
-    } else {
-      return (
-        <Link className="header__entry" to="/signin" onClick={toggleBtnState}>
-          Войти
-        </Link>
-      );
-    }
+  // function renderBtn() {
+  //   if (btnState) {
+  //     return (
+  //       <Link className="header__entry" to="/signup" onClick={toggleBtnState}>
+  //         Регистрация
+  //       </Link>
+  //     );
+  //   } else if (loggedIn) {
+  //     return (
+  //       <button
+  //         className={`header__menu ${isMenuOpen ? "header__menu_close" : ""}`}
+  //         type="button"
+  //         aria-label="меню"
+  //         onClick={toggleBtnMenu}
+  //       />
+  //     );
+  //   } else {
+  //     return (
+  //       <Link className="header__entry" to="/signin" onClick={toggleBtnState}>
+  //         Войти
+  //       </Link>
+  //     );
+  //   }
+  // }
+
+  function toggleBtnMenu() {
+    setIsMenuOpen(!isMenuOpen);
   }
+
   return (
     <header className="header root__content">
-      <button type="button" className="header__link">
-        <img
-          className="header__logo"
-          src={headerLogo}
-          alt="Логотип-надпись место Россия"
-        />
-      </button>
-      {loggedIn && <p className="header__user-email">{userEmail}</p>}
-      <>{renderBtn()}</>
+      {loggedIn && (
+        <nav
+          className={`header__navigation ${
+            isMenuOpen ? "header__navigation_opened" : ""
+          }`}
+        >
+          {loggedIn && <p className="header__user-email">{userEmail}</p>}
+          <Link className="header__entry" onClick={signOut}>
+            Выйти
+          </Link>
+        </nav>
+      )}
+      <div className="header__container">
+        <button type="button" className="header__link">
+          <img
+            className="header__logo"
+            src={headerLogo}
+            alt="Логотип-надпись место Россия"
+          />
+        </button>
+        {location.pathname === "/signin" && (
+          <Link className="header__entry" to="/signup">
+            Регистрация
+          </Link>
+        )}
+        {location.pathname === "/signup" && (
+          <Link className="header__entry" to="/signin">
+            Войти
+          </Link>
+        )}
+        {/* {renderBtn()} */}
+      </div>
     </header>
   );
 }
